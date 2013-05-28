0bfeeae06915d66b1fc7bb90498201e8f39e42db
rodrigo <rodrigo@rodrigo-MS-7529.(none)>
2013-05-28 09:07:56 -0300

    Colocando o PaperClip

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,3 +1,4 @@
 class Album < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :avatar
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
 end
