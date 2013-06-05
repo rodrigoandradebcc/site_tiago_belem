a3bc8a0ac756f5b5380cd4136b1b6148be7517a6
cesupa <cesupa@rodrigo-VirtualBox.(none)>
2013-06-05 14:30:43 -0300

    projeto do 0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteTiagoBelem::Application.config.secret_token = '7965cd9e5ca7d9f3a83f43df04dfddda79c04c654790b3745d518d308f7b0e7aabadf153e924d6f0c9e96f467788df2c6e91c143d8a2b8eccaca3c9f23c8d94f'
